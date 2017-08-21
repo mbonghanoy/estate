9312f6967b44717ac9ceba978d85ce1bed208b4e
mbonghanoy <bonghanoymark@gmail.com>
2017-08-21 20:16:29 +0800

    added login layout

--- views/login.php
+++ views/login.php
@@ -2,20 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/items.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <script src="https://use.fontawesome.com/215138297b.js"></script>
+    <title>Dashboard</title>
 </head>
 <body>
-    <form action="" method="POST">
-        <div>
-            <td><input type="text" name="username"></td>
-        </div>
-        <div>
-            <td><input type="text" name="password"></td>
-        </div>
-        <div>
-            <td><input type="submit" name="login" value="login"></td>
-        </div>
-    </form>
-
+    <div class="parent">
+        <form class="form-login" method="POST">
+            <div>
+                Username
+                <input type="text" name="username">
+            </div>
+            <div>
+                Password
+                <input type="text" name="password">
+            </div>
+            <div>
+                <button type="submit" name="login">Login</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
